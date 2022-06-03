af37bab0b159dc5da673ec624a878f71783b0236
Gabriel Roldan <gabriel.roldan@gmail.com>
2022-06-02 21:12:31 -0300

    Use rawCatalog instead of catalog in ModificationProxy.replaceCatalogInfo()
    ModificationProxy.replaceCatalogInfo() obtains the Catalog
    from GeoServerExtensions, and is private method used when cloning
    a CatalogInfo object. Using the catalog implies going through
    the whole chain of Catalog decorators, which includes
    AdvertisedCatalog, LocalWorkspaceCatalog, SecuredCatalog, etc.
    
    Besides the fact that this process shouldn't care about all that,
    the actual issue is it's called by ResourcePool every time
    a DataStore is requested, then some CatalogInfo references
    need to be resolved by ResolvingProxy, which calls the Catalog,
    and the decorators end up modifying the Filter used to do so,
    which in turn precludes catalog backends from optimizing the
    query by id because the filter is too complex.

--- src/main/src/main/java/org/geoserver/catalog/impl/ModificationProxy.java
+++ src/main/src/main/java/org/geoserver/catalog/impl/ModificationProxy.java
@@ -567,7 +567,7 @@ public class ModificationProxy implements WrappingProxy, Serializable {
 
     private CatalogInfo replaceCatalogInfo(CatalogInfo ci) {
         String id = ci.getId();
-        Catalog catalog = (Catalog) GeoServerExtensions.bean("catalog");
+        Catalog catalog = (Catalog) GeoServerExtensions.bean("rawCatalog");
         FilterFactory ff = CommonFactoryFinder.getFilterFactory();
         Class<? extends CatalogInfo> iface = getCatalogInfoInterface(ci.getClass());
         CatalogInfo replacement =
